a1dc97bb37ebd44b8dd8462d3ff0f6a1e5b5cee9
brendanahart <hbrendan@umich.edu>
2015-11-03 16:43:47 -0500

    Revert "Form Validation Final"
    This reverts commit e6095c175c3d2524c92f5430ca26591fecdc0d5a.
    
    Conflicts:
    	js/index.js

--- js/index.js
+++ js/index.js
@@ -1,50 +1,81 @@
-
-// Sign Up Form Validation! 
 var info = function() { 
 	$('.information-fill').find('#formBTN').click(function() {
 
-		var inputs = document.getElementById("Validation").elements;
-		var inputsLength = inputs.length; 
-		var correctInputs = 0; 
-		for(var i = 0; i < (inputsLength - 1); i++)
+		$('.infomration-fill').find('.section-heading').hide(); 
+
+		var fName = document.getElementById("inputFirstName");
+		var lName = document.getElementById("inputLastName");
+		var email = document.getElementById("inputEmail");
+		var community = document.getElementById("inputCommunity"); 
+		var youStatus = document.getElementById("youInput")
+		var country = document.getElementById("countryInput");
+		var state = document.getElementById("stateInput"); 
+
+		if(fName == null || fName == "") {
+			$('#inputFirstName').parent('.form-group').addClass(".has-error")
+			$('#inputFirstName').attr("placeholder", "*You Must Include Your First Name")
+		}
+		else
 		{
-			if (inputs[i].value == null || inputs[i].value == "")
-			{
-				// give id of current form value 
-				var currentValue = document.getElementById("Validation")[i].id;
-				var jQueryString = ("#" + currentValue); 
-				$(jQueryString).parents("div.form-group").addClass("has-error has-feedback");
-				$(jQueryString).siblings("span").addClass("glyphicon glyphicon-remove form-control-feedback");
-			}
-			else
-			{
-				correctInputs = correctInputs + 1; 
-				var currentValue = document.getElementById("Validation")[i].id;
-				var jQueryString = ("#" + currentValue); 
-				$(jQueryString).parents("div.form-group").removeClass("has-error");
-				$(jQueryString).siblings("span").removeClass("glyphicon glyphicon-remove form-control-feedback");
-				$(jQueryString).parents("div.form-group").addClass("has-success has-feedback");
-				$(jQueryString).siblings("span").addClass("glyphicon glyphicon-ok form-control-feedback");
-			}
+			$('#inputFirstName').parent('.form-group').addClass(".has-success")
 		}
 
-		if (correctInputs == (inputsLength - 1))
-		{
-			$("#formBTN").removeClass("btn-default");
-			$("#formBTN").removeClass("btn-warning");		
-			$("#formBTN").addClass("btn-success");
-			document.getElementById("formBTN").innerHTML = "Success!"
-
+		if(lName == null || lName == "") {
+			$('#inputLastName').parent('.form-group').addClass(".has-error")
+			$('#inputLastName').attr("placeholder", "*You Must Include Your Last Name")
 		}
-		else 
+		else
 		{
+			$('#inputLastName').parent('.form-group').addClass(".has-success")
+		}
+
+		if(email == null || email == "") {
+			$('#inputEmail').parent('.form-group').addClass(".has-error")
+			$('#inputEmail').attr("placeholder", "*You Must Include Your Email")
+		}
+		else
+		{
+			$('#inputEmail').parent('.form-group').addClass(".has-success")
+		}
+
+		if(community == null || community == "") {
+			$('#inputCommunity').parent('.form-group').addClass(".has-error")
+			$('#inputCommunity').attr("placeholder", "*You Must Include Your Community")
+		}
+		else
+		{
+<<<<<<< HEAD
 			$("#formBTN").addClass("btn-warning");		
 			$("#formBTN").removeClass("btn-success");
 			$("#formBTN").removeClass("btn-default");
 			document.getElementById("formBTN").innerHTML = "Error!"
 			return false; 
+=======
+			$('#inputCommunity').parent('.form-group').addClass(".has-success")
+>>>>>>> parent of e6095c1... Form Validation Final
 		}
+
+		if(youStatus == null || youStatus == "") {
+			$('#youInput').parent('.form-group').addClass(".has-error")
+			$('#youInput').attr("placeholder", "*You Must clarify if Your Status")
+		}
+		else
+		{
+			$('#youInput').parent('.form-group').addClass(".has-success")
+		}
+
+		if(state == null || state == "") {
+			$('#stateInput').parent('.form-group').addClass(".has-error")
+			$('#stateInput').attr("placeholder", "*You Must Include Your State For Validation")
+		}
+		else
+		{
+			$('#stateInput').parent('.form-group').addClass(".has-success")
+		}
+
 	});
+
 };
+
 
 $(document).ready(info);
