2cfe64907076bd3317d5fe2127da173d0c97a250
brendanahart <hbrendan@umich.edu>
2015-11-19 17:06:52 -0500

    Redirect after SignUp

--- js/users.js
+++ js/users.js
@@ -3,56 +3,6 @@ var myApp = angular.module('userApp', []);
 myApp.controller('UserController', function() {
 	var user = this; 
 
-	user.schools = []; 
-	user.roles = []
-
-	user.initialization = function()
-	{
-		var schools = Parse.Object.extend("Schools");
-		var querySchool = new Parse.Query(schools); 
-
-		querySchool.exists("SchoolName"); 
-
-		querySchool.find({
-			success: function(results) {
-				console.log("Successfully retrieved " + results.length + " scores."); 
-
-				for (var i = 0; i < results.length; i++)
-				{
-					var school = results[i];
-					user.schools[i] = school.get("SchoolName"); 
-				}
-			},
-			error: function(error)
-			{
-				console.log("Error: " + error.code + " " + error.message)
-			}
-		});
-
-		var relation = Parse.Object.extend("SchoolRelation");
-		var queryRelation = new Parse.Query(relation); 
-
-		queryRelation.exists("Relation"); 
-
-		queryRelation.find({
-			success: function(results) {
-				console.log("Successfully retrieved " + results.length + " scores."); 
-
-				for (var i = 0; i < results.length; i++)
-				{
-					var relation = results[i];
-					user.roles[i] = relation.get("Relation"); 
-				}
-			},
-			error: function(error)
-			{
-				console.log("Error: " + error.code + " " + error.message)
-			}
-		})
-	}
-
-	user.initialization(); 
-
 	user.signUp = function(firstName, lastName, email, password, confirmPassword)
 	{
 		if (password != confirmPassword)
@@ -80,11 +30,7 @@ myApp.controller('UserController', function() {
 		user.signUp(null, {
 			success: function(user) {
 		    	alert("You can now user the app now!"); 
-		    	user.firstName = "";
-		    	user.lastName = "";
-		    	user.email = "";
-		    	user.password = "";
-		    	user.confirmPassword = "";
+		    	window.location.href = "http://stuwork.com/whoareyou.html"
 		    },
 		    error: function(user, error) {
 		    	// Show the error message somewhere and let the user try again.
